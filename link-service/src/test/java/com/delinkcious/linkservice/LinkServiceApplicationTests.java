7faa0344c272fe9439750506c2562856e5b41b7f
준모 강 <boldfaced7@hanyang.ac.kr>
2023-05-03 13:36:45 +0900

    link-service 생성

---
+++ link-service/src/test/java/com/delinkcious/linkservice/LinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delinkcious.linkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
