3fc447fda317fafb21543f22a30e07c3bf5c5394
boldfaced7 <boldfaced753@gmail.com>
2023-05-04 23:07:04 +0900

    social-graph-service 생성

---
+++ soical-graph-service/src/main/java/com/delinkcious/soicalgraphservice/SoicalGraphServiceApplication.java
@@ -0,0 +1,13 @@
+package com.delinkcious.soicalgraphservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoicalGraphServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoicalGraphServiceApplication.class, args);
+	}
+
+}
---
+++ soical-graph-service/src/test/java/com/delinkcious/soicalgraphservice/SoicalGraphServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delinkcious.soicalgraphservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoicalGraphServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
