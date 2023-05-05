a99a938b4e2347755603b1e10a7b82e33e76224c
boldfaced7 <boldfaced753@gmail.com>
2023-05-05 15:18:18 +0900

    social-graph-service 생성

---
+++ social-graph-service/src/test/java/com/delinkcious/socialgraphservice/SocialGraphServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delinkcious.socialgraphservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialGraphServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
